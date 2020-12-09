6a665879d6a74fa8903eeaf1eb577216fa3cf488
sowmyatellapragada <63146223+sowmyatellapragada@users.noreply.github.com>
2020-12-09 18:02:44 +0530

    Add files via upload

---
+++ abschild.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp3
+{
+    class abschild:absparent
+
+    {
+        public override void Mul(int x, int y)
+        {
+            Console.WriteLine(x * y);
+        }
+        public override void div(int x, int y)
+        {
+            Console.WriteLine(x / y);
+        }
+        static void Main()
+        {
+            abschild c = new abschild();
+            absparent p = c;
+            p.Add(100, 100);p.sub(50, 50);p.Mul(10, 2);p.div(10, 2);
+        }
+
+
+    }
+}
---
+++ absparent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+   abstract class absparent
+    {
+        public void Add(int x, int y)
+        {
+            Console.WriteLine(x + y);
+        }
+        public void sub(int x,int y)
+        {
+            Console.WriteLine(x - y);
+        }
+        public abstract void Mul(int x, int y);
+        public abstract void div(int x, int y);
+       
+
+    }
+}
